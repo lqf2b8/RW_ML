32f43c9adff52ed24bee7dffbab00329c7a91e10
Lucas Fields <lqf2b8@gmail.com>
2026-01-14 16:46:21 -0600

    extract config parameters

--- services/trade_producer/src/kraken_websocket_api.py
+++ services/trade_producer/src/kraken_websocket_api.py
@@ -1,49 +1,129 @@
+from datetime import datetime, timezone
 from typing import List
+import json
 from websocket import create_connection
+
 from loguru import logger
 
-class KrakenWebSocketAPI:
-    URL = "wss://ws.kraken.com/"
+from pydantic import BaseModel
 
+class Trade(BaseModel):
+    product_id: str
+    quantity: float
+    price: float
+    timestamp_ms: int
+
+class KrakenWebsocketAPI:
 
     """
-    Class for reading trade data from Kraken via WebSocket API.
+    Class for reading real-time trades from the Kraken Websocket API
     """
+    URL = 'wss://ws.kraken.com/v2'
 
     def __init__(self, product_id: str):
+        """
+        Initializes the KrakenWebsocketAPI instance
+
+        Args:
+            product_id (str): The product id to get the trades from
+        """
         self.product_id = product_id
-        # Initialize WebSocket connection here
-        self.ws = create_connection(self.URL)
-        logger.info(f"Connected to Kraken WebSocket API at {self.URL}") 
 
-        self._subscribe(product_id=self.product_id)
+        # establish connection to the Kraken websocket API
+        self._ws = create_connection(self.URL)
+        logger.debug('Connection established')
 
-    def get_trades(self) -> List[dict]:
+        # subscribe to the trades for the given `product_id`
+        self._subscribe(product_id)
+
+    def get_trades(self) -> List[Trade]:
         """
-        Docstring for get_trades
-        
-        :param self: Description
-        :return: Description
-        :rtype: List[dict]
-        """
-        event = [{
-                "product_id": "ETH/USD",
-                "price": 1000,
-                "qty": 1,
-                "timestamp_ms": 163000000000,
-            }]
-        from time import sleep
-        sleep(1)
-        
-        return event
+        Returns the latest batch of trades from the Kraken Websocket API
 
-    
+        Args:
+            None
+        
+        Returns:
+            List[Trade]: A list of Trade objects
+        """
+        message = self._ws.recv()
+
+        if 'heartbeat' in message:
+            # when I get a heartbeat, I return an empty list
+            logger.debug('Heartbeat received')
+            return []
+
+        # parse the message string as a dictionary
+        message = json.loads(message)
+
+        # extract trades from the message['data'] field
+        trades = []
+        for trade in message['data']:
+            
+            # extract the following fields
+            # - product_id
+            # - quantity
+            # - price
+            # - timestamp in milliseconds
+            trades.append(
+                Trade(
+                    product_id=trade['symbol'],
+                    price=trade['price'],
+                    quantity=trade['qty'],
+                    timestamp_ms=self.to_ms(trade['timestamp']),
+                )
+            )
+
+        return trades
+
     def is_done(self) -> bool:
         """
-        Docstring for is_done
-        
-        :param self: Description
-        :return: Description
-        :rtype: bool
+        Returns True if the Kraken Websocket API connection is closed
         """
-        return False
+        False
+
+    def _subscribe(self, product_id: str):
+        """
+        Establish connection to the Kraken websocket API and subscribe to the trades for the given `product_id`.
+        """
+        logger.info(f'Subscribing to trades for {product_id}')
+        # let's subscribe to the trades for the given `product_id`
+        msg = {
+            'method': 'subscribe',
+            'params': {
+                'channel': 'trade',
+                'symbol': [product_id],
+                'snapshot': False,
+            },
+        }
+        self._ws.send(json.dumps(msg))
+        logger.info('Subscription worked!')
+
+        # For each product_id we dump
+        # the first 2 messages we got from the websocket, because they contain
+        # no trade data, just confirmation on their end that the subscription was successful
+        for product_id in [product_id]:
+            _ = self._ws.recv()
+            _ = self._ws.recv()
+
+    @staticmethod
+    def to_ms(timestamp: str) -> int:
+        """
+        A function that transforms a timestamps expressed
+        as a string like this '2024-06-17T09:36:39.467866Z'
+        into a timestamp expressed in milliseconds.
+
+        Args:
+            timestamp (str): A timestamp expressed as a string.
+
+        Returns:
+            int: A timestamp expressed in milliseconds.
+        """
+        # parse a string like this '2024-06-17T09:36:39.467866Z'
+        # into a datetime object assuming UTC timezone
+        # and then transform this datetime object into Unix timestamp
+        # expressed in milliseconds
+        
+
+        timestamp = datetime.fromisoformat(timestamp[:-1]).replace(tzinfo=timezone.utc)
+        return int(timestamp.timestamp() * 1000)
--- services/trade_producer/src/main.py
+++ services/trade_producer/src/main.py
@@ -1,52 +1,62 @@
+from typing import List
 
 from loguru import logger
 from quixstreams import Application
 
-from src.kraken_websocket_api import KrakenWebSocketAPI
-
+from src.kraken_websocket_api import (
+    KrakenWebsocketAPI,
+    Trade,
+)
 
 def produce_trades(
-        kafka_broker_address: str,
-        kafka_topic: str,
-        product_id: str,
+    kafka_broker_address: str,
+    kafka_topic: str,
+    product_id: str,
 ):
-    """ 
-    Reads trades from kraken websocket and produces them to a Kafka topic.
+    """
+    Reads trades from the Kraken Websocket API and saves them in the given `kafka_topic`
 
     Args:
-        kafka_broker_address (str): Address of the Kafka broker.
-        kafka_topic (str): Kafka topic to save trades to.
-        product_id (str): Product ID to filter trades for.
+        kafka_broker_address (str): The address of the Kafka broker
+        kafka_topic (str): The Kafka topic to save the trades
+        product_id (str): The product id to get the trades from
 
-        Returns:
-            None
-    """
-
+    Returns:
+        None
+    """    
+    # Create an Application instance with Kafka config
     app = Application(broker_address=kafka_broker_address)
 
-    topic = app.topic(name=kafka_topic, value_serializer="json")
-    
-    # Create krankan api object
-    kraken_api = KrakenWebSocketAPI(product_id=product_id)
+    # Define a topic "my_topic" with JSON serialization
+    topic = app.topic(name=kafka_topic, value_serializer='json')
 
+    # create a kraken api object
+    kraken_api = KrakenWebsocketAPI(product_id=product_id)
+
+    # Create a Producer instance
     with app.get_producer() as producer:
-        #kraken_trades_stream = app.get_data_streams().from_kraken_trades(product_id=product_id)
-        #kraken_trades_stream.save_to_topic(producer=producer, topic=topic)
 
         while True:
 
-            trades = kraken_api.get_trades()
-
+            trades: List[Trade] = kraken_api.get_trades()
+            
             for trade in trades:
-                message = topic.serialize(key=trade["product_id"], value=trade)
-                producer.produce(topic=topic.name, key=message.key, value=message.value)
-                logger.info(f"Produced trade: {trade}")
+                # Serialize an event using the defined Topic
+                # transform it into a sequence of bytes
+                message = topic.serialize(key=trade.product_id, value=trade.model_dump())
                 
+                # Produce a message into the Kafka topic
+                producer.produce(topic=topic.name, value=message.value, key=message.key)
+
+                logger.debug(f"Pushed trade to Kafka: {trade}")
 
 
 if __name__ == "__main__":
+    
+    from src.config import config
+
     produce_trades(
-        kafka_broker_address="localhost:19092",
-        kafka_topic="trades",
-        product_id="ETH/USD",
+        kafka_broker_address=config.kafka_broker_address,
+        kafka_topic=config.kafka_topic,
+        product_id=config.product_id,
     )
